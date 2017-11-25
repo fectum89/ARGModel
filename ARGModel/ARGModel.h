d2beb604ed26dd3462a9cf2636fd34873225b3e0
Sergey Polshcha <fectum.developer@gmail.com>
2017-11-25 20:44:55 +0700

    initial commit

---
+++ ARGModel/ARGModel.h
@@ -0,0 +1,19 @@
+//
+//  ARGModel.h
+//  ARGModel
+//
+//  Created by Admin on 25/11/2017.
+//  Copyright © 2017 Argentum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARGModel.
+FOUNDATION_EXPORT double ARGModelVersionNumber;
+
+//! Project version string for ARGModel.
+FOUNDATION_EXPORT const unsigned char ARGModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARGModel/PublicHeader.h>
+
+
