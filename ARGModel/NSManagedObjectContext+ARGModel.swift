8c18accbd2b1202da85c3426c70e8965e17a07fb
Fectum <fectum.developer@gmail.com>
2018-05-25 13:15:59 +0700

    fixes

--- ARGModel/NSManagedObjectContext+ARGModel.swift
+++ ARGModel/NSManagedObjectContext+ARGModel.swift
@@ -11,8 +11,7 @@ import Foundation
 extension NSManagedObjectContext {
     
     open func create<T: NSManagedObject>(_ type: T.Type) -> T {
-        let entityName = ARGModel.shared.preferences?.entityMapping?(NSStringFromClass(type)) ?? NSStringFromClass(type)
-        let components = entityName.components(separatedBy: ".")
+        let components = entityName(for: type).components(separatedBy: ".")
         let object = NSEntityDescription.insertNewObject(forEntityName: components.last!, into: self) as! T
         object.assignToConfiguration("PF_DEFAULT_CONFIGURATION_NAME");
         object.onCreate()
@@ -29,8 +28,7 @@ extension NSManagedObjectContext {
     }
     
     public func fetchObjects<T: NSManagedObject>(type: T.Type, predicate: NSPredicate?) -> [T]? {
-        let entityName = ARGModel.shared.preferences?.entityMapping?(NSStringFromClass(type)) ?? NSStringFromClass(type)
-        let request = NSFetchRequest<T>(entityName: entityName)
+        let request = NSFetchRequest<T>(entityName: entityName(for: type))
         
         request.predicate = predicate
         
@@ -43,22 +41,40 @@ extension NSManagedObjectContext {
         return []
     }
     
-    public func fetchObjects<T: NSManagedObject>(type: T.Type, _ configureRequestClosure: (NSFetchRequest<T>) -> ()) -> [T]? {
-        let entityName = ARGModel.shared.preferences?.entityMapping?(NSStringFromClass(type)) ?? NSStringFromClass(type)
-        let request = NSFetchRequest<T>(entityName: entityName)
-        
-        configureRequestClosure(request)
-        
-        //request.predicate = predicate
+    public func countOfObjects<T: NSManagedObject>(type: T.Type) -> Int {
+        //let entityName = ARGModel.shared.preferences?.entityMapping?(NSStringFromClass(type)) ?? NSStringFromClass(type)
+        let request = NSFetchRequest<T>(entityName: entityName(for: type))
         
         do {
-            return try self.fetch(request)
+            return try self.count(for: request)
         } catch {
             print(error)
         }
         
-        return []
+        return 0
     }
+    
+    public func entityName<T: NSManagedObject>(for type: T.Type) -> String {
+        let entityName = ARGModel.shared.preferences?.entityMapping?(NSStringFromClass(type)) ?? NSStringFromClass(type)
+        return entityName
+    }
+    
+//    public func fetchObjects<T: NSManagedObject>(type: T.Type, _ configureRequestClosure: (NSFetchRequest<T>) -> ()) -> [T]? {
+//        let entityName = ARGModel.shared.preferences?.entityMapping?(NSStringFromClass(type)) ?? NSStringFromClass(type)
+//        let request = NSFetchRequest<T>(entityName: entityName)
+//
+//        configureRequestClosure(request)
+//
+//        //request.predicate = predicate
+//
+//        do {
+//            return try self.fetch(request)
+//        } catch {
+//            print(error)
+//        }
+//
+//        return []
+//    }
     
     public func objectForID<T: NSManagedObject>(_ objectId: NSManagedObjectID, type: T.Type) -> T? {
         do {
@@ -100,9 +116,14 @@ public extension NSManagedObjectContext {
     }
     
     @objc
-    public func fetchObjects(of type: AnyClass, configureBlock configureRequestClosure: (NSFetchRequest<NSManagedObject>) -> ()) -> [Any]? {
-        return self.fetchObjects(type: type as! NSManagedObject.Type, configureRequestClosure)
+    public func count(of: AnyClass) -> Int {
+        return self.countOfObjects(type: of as! NSManagedObject.Type)
     }
+    
+//    @objc
+//    public func fetchObjects(of type: AnyClass, configureBlock configureRequestClosure: (NSFetchRequest<NSManagedObject>) -> ()) -> [Any]? {
+//        return self.fetchObjects(type: type as! NSManagedObject.Type, configureRequestClosure)
+//    }
     
     @objc public func objects(ids: [NSManagedObjectID]) -> [NSManagedObject] {
         return objectsWith(ids: ids)
